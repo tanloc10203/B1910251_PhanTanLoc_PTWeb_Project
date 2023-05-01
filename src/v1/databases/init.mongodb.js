715b47b4b7adf75dd641a16eb41a423f60f4b14a
Phan Tấn Lộc <ginga550504@gmail.com>
2023-05-01 19:12:14 +0700

    update mongo deploy

--- src/v1/databases/init.mongodb.js
+++ src/v1/databases/init.mongodb.js
@@ -4,7 +4,10 @@ mongoose.set("strictQuery", false);
 
 //connect mongoose
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then((_) => console.log("Connected mongoose success!..."))
   .catch((err) => console.error(`Error: connect:::`, err));
 
